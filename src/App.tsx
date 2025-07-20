d3596521bb37043c6cac8809c6ef8d921e073dc9
Klayton Junior <55154107+KlaytonJr@users.noreply.github.com>
2025-07-20 10:08:59 -0300

    chore: setup personalized font

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import "./styles/global.css";
+import {
+  useFonts,
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+} from "@expo-google-fonts/host-grotesk";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  let [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View className="bg-lime-500 flex-1 items-center justify-center">
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className="text-base font-sans-semibold">
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
