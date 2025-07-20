0d05cc2adb1630573008525c4e9f5e33a58927be
Klayton Junior <55154107+KlaytonJr@users.noreply.github.com>
2025-07-20 16:56:09 -0300

    feat: connect react hook form on signin

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 interface IAuthLayoutProps {
   icon: string;
   title: string;
   subtitle: string;
   children?: React.ReactNode;
 }
+
 export function AuthLayout({
   icon,
   title,
@@ -13,32 +15,29 @@ export function AuthLayout({
   children,
 }: IAuthLayoutProps) {
   const { bottom } = useSafeAreaInsets();
+
   return (
     <View className="flex-1">
-      {" "}
       <View className="flex-1 bg-white">
-        {" "}
-        <View className="h-[98px] bg-lime-400" />{" "}
+        <View className="h-[98px] bg-lime-400" />
+
         <View className="size-12 bg-white items-center justify-center rounded-xl -mt-6 mx-auto border-2 border-lime-400">
-          {" "}
-          <Text>{icon}</Text>{" "}
-        </View>{" "}
+          <Text>{icon}</Text>
+        </View>
+
         <View className="mx-auto mt-6 items-center">
-          {" "}
           <Text className="text-black-700 font-sans-semibold tracking-[-0.32px] text-[32px]">
-            {" "}
-            {title}{" "}
-          </Text>{" "}
+            {title}
+          </Text>
           <Text className="mt-1 text-gray-700 text-base font-sans-regular">
-            {" "}
-            {subtitle}{" "}
-          </Text>{" "}
-        </View>{" "}
+            {subtitle}
+          </Text>
+        </View>
+
         <View className="flex-1 mt-10 px-6" style={{ marginBottom: bottom }}>
-          {" "}
-          {children}{" "}
-        </View>{" "}
-      </View>{" "}
+          {children}
+        </View>
+      </View>
     </View>
   );
 }
