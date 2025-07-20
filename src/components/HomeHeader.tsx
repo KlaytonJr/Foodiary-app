e80422a13a716a49f9017fa6a045cedfc9b0cfd9
Klayton Junior <55154107+KlaytonJr@users.noreply.github.com>
2025-07-20 19:29:17 -0300

    feat: load logged user

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,9 +1,12 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { LogOutIcon } from "lucide-react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "../hooks/useAuth";
 import { colors } from "../styles/colors";
 
 export function HomeHeader() {
+  const { signOut, user } = useAuth();
+
   return (
     <View className="bg-lime-400 h-[130px]">
       <SafeAreaView className="px-4 flex-row items-center justify-between">
@@ -12,12 +15,15 @@ export function HomeHeader() {
             Olá, 👋
           </Text>
           <Text className="text-black-700 text-base font-sans-semibold">
-            Klayton
+            {user?.name}
           </Text>
         </View>
 
-        <TouchableOpacity className="size-12 items-center justify-center">
-          <LogOutIcon size={30} color={colors.black[700]} />
+        <TouchableOpacity
+          className="size-12 items-center justify-center"
+          onPress={signOut}
+        >
+          <LogOutIcon size={20} color={colors.black[700]} />
         </TouchableOpacity>
       </SafeAreaView>
     </View>
