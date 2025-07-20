b48dcf137b267ac4bff731109e33a27b2877e6b5
Klayton Junior <55154107+KlaytonJr@users.noreply.github.com>
2025-07-20 15:15:06 -0300

    feat: implement auth context

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,3 @@
-import { Stack } from "expo-router";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import * as SplashScreen from "expo-splash-screen";
 import {
   HostGrotesk_400Regular,
   HostGrotesk_500Medium,
@@ -8,12 +5,30 @@ import {
   HostGrotesk_700Bold,
   useFonts,
 } from "@expo-google-fonts/host-grotesk";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { AuthProvider } from "../contexts/AuthContext";
+import { useAuth } from "../hooks/useAuth";
 import "../styles/global.css";
 
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+export default function Layout() {
+  return (
+    <SafeAreaProvider>
+      <AuthProvider>
+        <RootLayout />
+      </AuthProvider>
+    </SafeAreaProvider>
+  );
+}
+
+function RootLayout() {
+  const { isLoggedIn, isLoading } = useAuth();
+
   const [loaded, error] = useFonts({
     HostGrotesk_400Regular,
     HostGrotesk_500Medium,
@@ -22,7 +37,10 @@ export default function RootLayout() {
   });
 
   useEffect(() => {
-    if (loaded || error) {
+    const isFontLoaded = loaded || error;
+    const isUserLoaded = !isLoading;
+
+    if (isFontLoaded && isUserLoaded) {
       SplashScreen.hideAsync();
     }
   }, [loaded, error]);
@@ -31,19 +49,15 @@ export default function RootLayout() {
     return null;
   }
 
-  const isLoggedIn = false;
-
   return (
-    <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Protected guard={isLoggedIn}>
-          <Stack.Screen name="(private)" />
-        </Stack.Protected>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={isLoggedIn}>
+        <Stack.Screen name="(private)" />
+      </Stack.Protected>
 
-        <Stack.Protected guard={!isLoggedIn}>
-          <Stack.Screen name="(public)" />
-        </Stack.Protected>
-      </Stack>
-    </SafeAreaProvider>
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen name="(public)" />
+      </Stack.Protected>
+    </Stack>
   );
 }
